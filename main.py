65ee62b8050eb09a4e1c06c83da14d08199a626d
Lala <larissa.santosngtb@gmail.com>
2026-05-25 16:49:28 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+
+
+print("Setup Start")
+#ctrl+alt+l --- organiza o código de acordo com a pep(retira spaces)
+pygame.init()
+window = pygame.display.set_mode(size=(800,480))
+print("Setup End")
+
+
+print("Loop Start")
+while True:
+    # Check for all events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit() # Close window
+            quit()
+
