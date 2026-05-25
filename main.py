ea1a2c2f686af66f0485178ce04f6e4585167770
Lala <larissa.santosngtb@gmail.com>
2026-05-25 18:44:05 -0300

    Creating new classes

--- main.py
+++ main.py
@@ -1,19 +1,4 @@
-import pygame
+from code.game import Game
 
-
-print("Setup Start")
-#ctrl+alt+l --- organiza o código de acordo com a pep(retira spaces)
-pygame.init()
-window = pygame.display.set_mode(size=(800,480))
-print("Setup End")
-
-
-print("Loop Start")
-while True:
-    # Check for all events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            print("Quit..")
-            pygame.quit() # Close window
-            quit()
-
+game = Game()
+game.run()
